f50bf6bbff3c108bedd5e06e1d2d17a91fb44f5b
kirill <apuc06@mail.ru>
2017-09-29 16:07:05 +0300

    victory scenario

--- js/learn.js
+++ js/learn.js
@@ -4,6 +4,16 @@
 document.addEventListener('DOMContentLoaded', function(){
     var ttt = new Ttt();
 
-    ttt.initBoard('tttWrap', {width:'350px', backgroundColorCell:'Yellow'});
+    ttt.initBoard('tttWrap', {
+        width:'350px',
+        backgroundColorCell:'Yellow',
+        backgroundColorCellVictory:'RED',
+        customVictoryScenario: function (type, title) {
+            var msg = document.createElement('div');
+            msg.innerHTML = 'Победили: ' + (type === 1 ? 'Крестики' : 'Нолики');
+            title.appendChild(msg);
+            console.log(title);
+        }
+    });
 });
 
